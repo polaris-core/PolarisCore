972271b2db0445e9c25860c33c80469dfa2c5c45
TonimatasDEV <tonimatasdev@proton.me>
2024-09-03 15:14:36 +0200

    Server now start.

--- src/main/java/dev/photoncore/server/Main.java
+++ src/main/java/dev/photoncore/server/Main.java
@@ -1,5 +1,6 @@
 package dev.photoncore.server;
 
+import net.minestom.server.MinecraftServer;
 import org.fusesource.jansi.AnsiConsole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,9 @@ public class Main {
 
     public static void main(String[] args) {
         AnsiConsole.systemInstall();
-        LOGGER.info("Hello World!");
+        LOGGER.info("Starting server...");
+        MinecraftServer server = MinecraftServer.init();
+        
+        server.start("0.0.0.0", 25565);
     }
 }

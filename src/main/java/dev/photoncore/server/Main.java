42fd052a19627245b700ab2e2d77aa9f85724807
TonimatasDEV <tonimatasdev@proton.me>
2024-09-03 15:08:09 +0200

    Add colors

--- src/main/java/dev/photoncore/server/Main.java
+++ src/main/java/dev/photoncore/server/Main.java
@@ -1,5 +1,6 @@
 package dev.photoncore.server;
 
+import org.fusesource.jansi.AnsiConsole;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -7,6 +8,7 @@ public class Main {
     public static Logger LOGGER = LoggerFactory.getLogger(Main.class);
 
     public static void main(String[] args) {
+        AnsiConsole.systemInstall();
         LOGGER.info("Hello World!");
     }
 }

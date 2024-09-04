bc33672c663dffd805c2a79f9f8026b8c7406db6
TonimatasDEV <tonimatasdev@proton.me>
2024-09-04 19:27:57 +0200

    Fence/Panels place

---
+++ src/main/java/dev/photoncore/server/blocks/placements/ConnectedPlacementRule.java
@@ -0,0 +1,53 @@
+package dev.photoncore.server.blocks.placements;
+
+import net.minestom.server.coordinate.Point;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.instance.block.BlockFace;
+import net.minestom.server.instance.block.rule.BlockPlacementRule;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.HashMap;
+import java.util.Locale;
+import java.util.Map;
+
+public class ConnectedPlacementRule extends BlockPlacementRule {
+    public ConnectedPlacementRule(@NotNull Block block) {
+        super(block);
+    }
+
+    @Override
+    public int maxUpdateDistance() {
+        return 1;
+    }
+
+    @Override
+    public @NotNull Block blockUpdate(@NotNull UpdateState updateState) {
+        return updateState.currentBlock().withProperties(getProperties(updateState.instance(), updateState.blockPosition()));
+    }
+
+    @Override
+    public @Nullable Block blockPlace(@NotNull PlacementState placementState) {
+        
+        
+        return placementState.block().withProperties(getProperties(placementState.instance(), placementState.placePosition()));
+    }
+    
+    public static Map<String, String> getProperties(Block.Getter instance, Point point) {
+        Map<String, String> properties = new HashMap<>();
+
+        for (BlockFace face : BlockFace.values()) {
+            if (face == BlockFace.TOP || face == BlockFace.BOTTOM) continue;
+
+            Block faceBlock = instance.getBlock(point.relative(face));
+
+            if (faceBlock.isSolid()) {
+                properties.put(face.name().toLowerCase(Locale.ENGLISH), "true");
+            } else {
+                properties.put(face.name().toLowerCase(Locale.ENGLISH), "false");
+            }
+        }
+        
+        return properties;
+    }
+}

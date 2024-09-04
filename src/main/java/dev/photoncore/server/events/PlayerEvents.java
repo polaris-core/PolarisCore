1ee61088c20a65629bb4c58457524b710f8a09fb
TonimatasDEV <tonimatasdev@proton.me>
2024-09-04 03:53:02 +0200

    Literally fix BlockHandlers

--- src/main/java/dev/photoncore/server/events/PlayerEvents.java
+++ src/main/java/dev/photoncore/server/events/PlayerEvents.java
@@ -2,12 +2,16 @@ package dev.photoncore.server.events;
 
 import dev.photoncore.server.Main;
 import dev.photoncore.server.init.Levels;
+import net.minestom.server.MinecraftServer;
 import net.minestom.server.coordinate.Pos;
 import net.minestom.server.entity.GameMode;
 import net.minestom.server.entity.Player;
 import net.minestom.server.entity.PlayerSkin;
 import net.minestom.server.event.GlobalEventHandler;
 import net.minestom.server.event.player.AsyncPlayerConfigurationEvent;
+import net.minestom.server.event.player.PlayerBlockPlaceEvent;
+import net.minestom.server.instance.block.Block;
+import net.minestom.server.instance.block.BlockHandler;
 
 public class PlayerEvents {
     public static void init(GlobalEventHandler handler) {
@@ -22,6 +26,15 @@ public class PlayerEvents {
             player.setRespawnPoint(new Pos(0, 42, 0));
             player.setSkin(PlayerSkin.fromUsername(player.getUsername()));
             player.setGameMode(GameMode.CREATIVE);
+        });
+        
+        handler.addListener(PlayerBlockPlaceEvent.class, event -> {
+            Block block = event.getBlock();
+            BlockHandler blockHandler = MinecraftServer.getBlockManager().getHandler(block.namespace().toString());
+            
+            if (blockHandler != null) {
+                event.setBlock(block.withHandler(blockHandler));
+            }
         });
     }
 }
--- src/main/java/dev/photoncore/server/init/Blocks.java
+++ src/main/java/dev/photoncore/server/init/Blocks.java
@@ -8,21 +8,19 @@ import net.minestom.server.instance.block.Block;
 import net.minestom.server.instance.block.BlockHandler;
 import net.minestom.server.instance.block.BlockManager;
 
-import java.util.Objects;
-
 public class Blocks {
     public static void init() {
         BlockManager blockManager = MinecraftServer.getBlockManager();
         
         // Block handlers
-        registerHandler(Block.CRAFTING_TABLE, new CraftingTableHandler());
-        registerHandler(Block.FURNACE, new FurnaceHandler());
+        registerHandler(new CraftingTableHandler());
+        registerHandler(new FurnaceHandler());
         
         // Placement rules
         blockManager.registerBlockPlacementRule(new HorizontalPlacementRule(Block.FURNACE));
     }
 
-    private static void registerHandler(Block block, BlockHandler blockHandler) {
-        MinecraftServer.getBlockManager().registerHandler(block.namespace(), () -> Objects.requireNonNull(block.withHandler(blockHandler).handler()));
+    private static void registerHandler(BlockHandler blockHandler) {
+        MinecraftServer.getBlockManager().registerHandler(blockHandler.getNamespaceId(), () -> blockHandler);
     }
 }

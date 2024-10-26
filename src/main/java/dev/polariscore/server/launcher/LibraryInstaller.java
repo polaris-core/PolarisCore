d69b8ac88695391875c5c3367d090fe49094cb72
TonimatasDEV <tonimatasdev@proton.me>
2024-10-26 20:48:43 +0200

    Delete old libs or not auto installed files

--- src/main/java/dev/polariscore/server/launcher/LibraryInstaller.java
+++ src/main/java/dev/polariscore/server/launcher/LibraryInstaller.java
@@ -7,9 +7,12 @@ import java.io.InputStream;
 import java.net.HttpURLConnection;
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.jar.JarFile;
+import java.util.stream.Stream;
 
 @SuppressWarnings("ResultOfMethodCallIgnored")
 public class LibraryInstaller {
@@ -22,6 +25,7 @@ public class LibraryInstaller {
         int part = max / numberChars;
         long time = System.currentTimeMillis();
         List<JarFile> jarFiles = new ArrayList<>();
+        List<Path> installedPaths = new ArrayList<>();
 
         int now = 0;
         boolean update = true;
@@ -45,12 +49,15 @@ public class LibraryInstaller {
                     update = false;
                 }
 
-                if (!new File("libraries/" + jarPath + "/" + jarName).exists())
-                    downloadLibrary(repository + jarPath, jarPath, jarName);
+                Path path = Path.of("libraries/" + jarPath + "/" + jarName);
+                installedPaths.addAll(addPreviousFolders(path));
+                
+                if (!Files.exists(path)) downloadLibrary(repository + jarPath, jarPath, jarName);
 
                 JarFile jarFile = null;
+                
                 try {
-                    jarFile = new JarFile("libraries/" + jarPath + "/" + jarName);
+                    jarFile = new JarFile(path.toFile());
                 } catch (IOException ignored) {
                 }
 
@@ -69,8 +76,11 @@ public class LibraryInstaller {
             }
         }
 
+        
         System.out.println();
         System.out.println("All libraries downloaded/loaded successfully in " + (System.currentTimeMillis() - time) + "ms");
+        
+        deleteOtherFiles(installedPaths);
     }
 
     public static void printReplace(String newMessage) {
@@ -78,20 +88,23 @@ public class LibraryInstaller {
     }
 
     private static void downloadLibrary(String url, String jarDirectory, String jarName) {
+        new File("libraries/" + jarDirectory).mkdirs();
+        
         try {
             HttpURLConnection connection = (HttpURLConnection) new URI(url + "/" + jarName).toURL().openConnection();
+            
             if (connection.getResponseCode() == HttpURLConnection.HTTP_OK) {
                 InputStream inputStream = connection.getInputStream();
-                new File("libraries/" + jarDirectory).mkdirs();
                 FileOutputStream outputStream = new FileOutputStream("libraries/" + jarDirectory + "/" + jarName);
                 byte[] buffer = new byte[1024];
                 int bytesRead;
+                
                 while ((bytesRead = inputStream.read(buffer)) != -1) {
                     outputStream.write(buffer, 0, bytesRead);
                 }
+                
                 outputStream.close();
                 inputStream.close();
-                //LOGGER.info("Library downloaded: " + jarName);
             }
         } catch (IOException | URISyntaxException e) {
             System.out.println("Error on download library: " + jarName);
@@ -100,5 +113,34 @@ public class LibraryInstaller {
     
     private static String getPercentage(int now, int max) {
         return (int) ((double) now / max * 100) + "%";
+    }
+    
+    private static void deleteOtherFiles(List<Path> installedPaths) {
+        try {
+            Stream<Path> paths = Files.walk(Path.of("libraries"));
+            
+            for (Path path : paths.toList()) {
+                if (!installedPaths.contains(path)) {
+                    Files.deleteIfExists(path);
+                }
+            }
+
+            paths.close();
+        } catch (IOException e) {
+            System.out.println("Error deleting a file not installed by the core in the libraries folder. " + e.getMessage());
+        }
+    }
+    
+    private static List<Path> addPreviousFolders(Path path) {
+        List<Path> paths = new ArrayList<>();
+        Path previous = path.getParent();
+        
+        if (previous != null) {
+            paths.addAll(addPreviousFolders(previous));
+        }
+        
+        paths.add(path);
+        
+        return paths;
     }
 }
